8a3dccf8022c2b75dbf1f8bea1355669621dabc3
Ammon Shepherd <ammon.shepherd@gmail.com>
2024-01-01 17:59:27 -0500

    Week 04 Learning Activity (1 of 2): HTML Forms
    Account: Process Registration

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -10,4 +10,7 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to build registration view
 router.get("/registration", utilities.handleErrors(accountController.buildRegister));
 
+// Route to post the new account data
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
 module.exports = router;
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const baseController = require("./controllers/baseController")
 const utilities = require("./utilities/")
 const session = require("express-session")
 const pool = require('./database/')
+const bodyParser = require("body-parser")
 
 /* ***********************
  * Middleware
@@ -38,6 +39,9 @@ app.use(function(req, res, next){
   next()
 })
 
+// parse the input values from the form inputs
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine and Templates
